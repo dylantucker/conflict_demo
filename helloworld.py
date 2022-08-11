ed6b9e367b6a599a7c000f278e647e9c1fe7e845
Dylan Tucker <dylan.tucker12@gmail.com>
2022-08-11 14:03:52 -0400

    removed repeated code

--- helloworld.py
+++ helloworld.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD
 def hello_world(city, state):
     print(f'Hello World {city}, {state}!')
 
-
 def main():
     hello_world('NYC', 'New York')
-=======
-def hello_world(city):
-    print(f'Hello World! {city}')
-
-
-def main():
-    hello_world('NYC')
->>>>>>> f6f0b1f7adbc34185bed5267292676b8fc6c5342
-
 
 main()
